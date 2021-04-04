b59f641221d175382da1c4ec7f5e5478cd01de60
Matt Mueller <54808875+matthewlouismueller@users.noreply.github.com>
2021-04-04 19:33:32 -0400

    Added Arduino IoT Interface

---
+++ ArduinoIoT-SpatialEdgeServer.ino
@@ -0,0 +1,214 @@
+/*
+  WiFi Web Server LED Blink
+
+ A simple web server that lets you blink an LED via the web.
+ This sketch will print the IP address of your WiFi module (once connected)
+ to the Serial monitor. From there, you can open that address in a web browser
+ to turn on and off the LED on pin 9.
+
+ If the IP address of your board is yourAddress:
+ http://yourAddress/H turns the LED on
+ http://yourAddress/L turns it off
+
+ This example is written for a network using WPA encryption. For
+ WEP or WPA, change the Wifi.begin() call accordingly.
+
+ Circuit:
+ * Board with NINA module (Arduino MKR WiFi 1010, MKR VIDOR 4000 and UNO WiFi Rev.2)
+ * LED attached to pin 9
+
+ created 25 Nov 2012
+ by Tom Igoe
+ */
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <ArduinoJson.h>
+#include <Arduino_LSM6DS3.h>
+
+#include "arduino_secrets.h" 
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = "RPiSpatialToolbox";        // your network SSID (name)
+char pass[] = "Vuforia123";    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;                 // your network key Index number (needed only for WEP)
+
+int status = WL_IDLE_STATUS;
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(9600);      // initialize serial communication
+  pinMode(13, OUTPUT);      // set the LED pin mode
+
+  // 
+  // init wifi
+  //
+  
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to Network named: ");
+    Serial.println(ssid);                   // print the network name (SSID);
+
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  server.begin();                           // start the web server on port 80
+  printWifiStatus();                        // you're connected now, so print out the status
+
+  //
+  // init Accelerometer
+  //
+  
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+  Serial.print("Accelerometer sample rate = ");
+  Serial.print(IMU.accelerationSampleRate());
+  Serial.println(" Hz");
+  Serial.println();
+  Serial.println("Acceleration in G's");
+  Serial.println("X\tY\tZ");
+  
+}
+
+StaticJsonDocument<200> doc;
+
+float x, y, z;
+long rssi;
+
+void loop() {
+  WiFiClient client = server.available();   // listen for incoming clients
+
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(x, y, z);
+  }
+  rssi = WiFi.RSSI();
+
+  doc["AccelX"] = x;
+  doc["AccelY"] = y;
+  doc["AccelZ"] = z;
+  doc["SignalStrength"] = rssi;
+
+  
+
+  if (client) {                             // if you get a client,
+    Serial.println("new client");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    String payload = "";
+    String pinListener = "";
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        if (c == '\n') {                    // if the byte is a newline character
+
+// if the current line is blank, you got two newline characters in a row.
+// that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+// HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+// and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:application/json");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            serializeJsonPretty(doc, client);
+
+            // The HTTP response ends with another blank line:
+            client.println();
+
+// Body of POST request comes here
+            while(client.available())
+              {
+                char d = client.read();             // read a byte, then
+                payload += d;
+//                  Serial.write(client.read());
+              }
+//              Serial.println(payload);
+              StaticJsonDocument<200> body;
+              DeserializationError error = deserializeJson(body, payload);
+              // Test if parsing succeeds.
+              if (error) {
+                Serial.print(F("deserializeJson() failed: "));
+                Serial.println(error.f_str());
+                return;
+              }
+
+            int led = body["13"];
+            Serial.print("led val: ");
+            Serial.println(led);
+            if (led) {
+              digitalWrite(13, HIGH);               // GET /H turns the LED on
+            }
+            else {
+              digitalWrite(13, LOW);
+            }
+            
+            // break out of the while loop:
+            break;
+          } else {    // if you got a newline, then clear currentLine:
+//            Serial.println();
+//            Serial.println(currentLine);
+//            Serial.println();
+                    // Check to see if the client request was "GET /H" or "GET /L":
+//              if (currentLine.startsWith("GET")) {
+//                digitalWrite(13, HIGH);               // GET /H turns the LED on
+//              }
+//              if (currentLine.startsWith("POST")) {
+//                digitalWrite(13, LOW);                // GET /L turns the LED off
+////                int Pin = currentLine.substring(currentLine.indexOf("/"),currentLine.lastIndexOf("/")).toInt();
+//                Serial.print("Pin data read: ");
+//                  
+//              }
+            currentLine = "";
+            payload = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("client disonnected");
+  }
+}
+
+//void parsePostCall() {
+//  int Pin = currentLine.substring(currentLine.indexOf("/"),currentLine.lastIndexOf("/")).toInt();
+//  Serial.print("Pin Number read: ");
+//  Serial.println(Pin);
+//}
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+  // print where to go in a browser:
+  Serial.print("To see this page in action, open a browser to http://");
+  Serial.println(ip);
+}
---
+++ interfaces/arduinoNanoIoT/index.js
@@ -0,0 +1,135 @@
+var server = require('@libraries/hardwareInterfaces');
+var settings = server.loadHardwareInterface(__dirname);
+
+const fetch = require('node-fetch');
+
+exports.enabled = settings('enabled');
+exports.configurable = true;
+
+var objectName = "ArduinoNanoIoT";
+var TOOL_NAME = "ArduinoNanoIoT1";
+
+var defaultIP = '192.168.1.180';
+
+sensorRefresh = 100;
+
+// set vars for input values from Arduino IoT
+var accelX;
+var accelY;
+var accelZ;
+var signalStrength;
+
+//
+// Function to send data from listener nodes and set values for writer nodes
+//
+var body; // sample payload for body of arduinoComm
+//
+async function arduinoComm(ip,payload) {
+    // if (isEmpty(payload)) {
+    //     console.log('set default payload')
+    //     payload = { '13': 1}; // set default payload
+    // }
+    fetch('http://'+ip, { 
+        method: 'POST', 
+        timeout: 5000,
+        body:    JSON.stringify(payload),
+        headers: { 'Content-Type': 'application/json' }
+    })
+    .then(res => res.text())
+    .then(json => {
+        // console.log(json)
+        return JSON.parse(json)
+    })
+    .then(body => {
+        accelX = body.AccelX;
+        accelY = body.AccelY;
+        accelZ = body.AccelZ;
+        signalStrength = body.SignalStrength;
+
+        server.write(objectName, TOOL_NAME, "accelerometerX", accelX, "f");
+        server.write(objectName, TOOL_NAME, "accelerometerY", accelY, "f");
+        server.write(objectName, TOOL_NAME, "accelerometerZ", accelZ, "f");
+        server.write(objectName, TOOL_NAME, "signalStrength", signalStrength, "f");
+    })
+    .catch(err => console.error(err));
+    setTimeout(() => { arduinoComm(IPaddress,body); }, sensorRefresh);
+    console.log(payload)
+}
+
+if (exports.enabled) {
+    setup();    
+
+    function setup() {
+        exports.settings = {
+            arduinoName: {
+                value: settings('objectName', objectName),
+                type: 'text',
+                default: objectName,
+                disabled: false,
+                helpText: 'The name of the object that connects to this hardware interface'
+            },
+            // Complexity level for the object
+            arduinoIP: { 
+                value: settings('Arduino IP Address', defaultIP), 
+                type: 'text',
+                default: defaultIP,
+                disabled: false,
+                helpText: 'The IP address of your Arduino Nano IoT on the same network as the Vuforia Edge Server'
+            }
+        };
+    }
+    objectName = exports.settings.arduinoName.value;
+    IPaddress = exports.settings.arduinoIP.value;
+    console.log("Arduino IoT: " + objectName);
+    console.log("with IP address: " + IPaddress);
+
+    server.addEventListener('reset', function () {
+        settings = server.loadHardwareInterface(__dirname);
+        setup();
+
+        console.log("Arduino IoT: Settings Loaded: ", objectName);
+    }) 
+}
+
+function startHardwareInterface() {
+    console.log('Arduino IoT: Starting up')
+
+    server.enableDeveloperUI(true)
+
+    server.addNode(objectName, TOOL_NAME, "accelerometerX", "node");
+    server.addNode(objectName, TOOL_NAME, "accelerometerY", "node");
+    server.addNode(objectName, TOOL_NAME, "accelerometerZ", "node");
+    server.addNode(objectName, TOOL_NAME, "signalStrength", "node");
+    server.addNode(objectName, TOOL_NAME, "led", "node");
+    
+    server.addReadListener(objectName, TOOL_NAME, "led", function(data){
+    	console.log("in read listener");
+    	if (data.value == 1) {
+    		// console.log("data is 1");
+    		setTimeout(() => { body = { '13': 1}; } , 0);
+    	}
+    	else if (data.value == 0) {
+    		// console.log("data is 0");
+    		setTimeout(() => { body = { '13': 0}; } , 0);
+    		// console.log("wrote to port");
+    	}
+    });
+
+    arduinoComm(IPaddress,body);
+
+    updateEvery(0, 1000);
+
+}
+
+function updateEvery(i, time){
+    setTimeout(() => {
+    	updateEvery(++i, time);
+    }, time)
+}
+
+server.addEventListener("initialize", function () {
+    if (exports.enabled) setTimeout(() => { 
+        body = { '13': 0};
+        startHardwareInterface();
+    }, 1000)
+});
