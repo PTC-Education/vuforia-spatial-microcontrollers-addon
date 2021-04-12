bdb41c64779b0f47fabd326e209cf77b95056c3d
Matt Mueller <54808875+matthewlouismueller@users.noreply.github.com>
2021-04-12 10:33:56 -0400

    Updated Arduino Script

--- ArduinoIoT-SpatialEdgeServer.ino
+++ ArduinoIoT-SpatialEdgeServer.ino
@@ -1,34 +1,24 @@
 /*
-  WiFi Web Server LED Blink
+  Code based on "WiFi Web Server LED Blink" example from WifiNinna library
 
- A simple web server that lets you blink an LED via the web.
- This sketch will print the IP address of your WiFi module (once connected)
- to the Serial monitor. From there, you can open that address in a web browser
- to turn on and off the LED on pin 9.
+  Wifi web server for interacting with Arduino IoT hardware interface for Vuforia Spatial Toolbox
+  
+  Written April, 2021
+  by Matt Mueller
 
- If the IP address of your board is yourAddress:
- http://yourAddress/H turns the LED on
- http://yourAddress/L turns it off
-
- This example is written for a network using WPA encryption. For
- WEP or WPA, change the Wifi.begin() call accordingly.
-
- Circuit:
- * Board with NINA module (Arduino MKR WiFi 1010, MKR VIDOR 4000 and UNO WiFi Rev.2)
- * LED attached to pin 9
-
- created 25 Nov 2012
- by Tom Igoe
  */
 #include <SPI.h>
 #include <WiFiNINA.h>
 #include <ArduinoJson.h>
 #include <Arduino_LSM6DS3.h>
 
+#include "arduino_secrets.h" 
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = "RPiSpatialToolbox";        // your network SSID (name)
 char pass[] = "Vuforia123";    // your network password (use for WPA, or use as key for WEP)
 int keyIndex = 0;                 // your network key Index number (needed only for WEP)
+
+String asd;
 
 int status = WL_IDLE_STATUS;
 WiFiServer server(80);
@@ -82,7 +72,7 @@ void setup() {
   
 }
 
-StaticJsonDocument<200> doc;
+StaticJsonDocument<500> doc;
 
 float x, y, z;
 long rssi;
@@ -134,8 +124,10 @@ void loop() {
                 payload += d;
 //                  Serial.write(client.read());
               }
-//              Serial.println(payload);
+              Serial.print("payload ");
+              Serial.println(payload);
               StaticJsonDocument<200> body;
+              
               DeserializationError error = deserializeJson(body, payload);
               // Test if parsing succeeds.
               if (error) {
@@ -144,32 +136,84 @@ void loop() {
                 return;
               }
 
-            int led = body["13"];
-            Serial.print("led val: ");
-            Serial.println(led);
-            if (led) {
-              digitalWrite(13, HIGH);               // GET /H turns the LED on
-            }
-            else {
-              digitalWrite(13, LOW);
-            }
+              JsonObject data = body.as<JsonObject>();
+              
+              for (int i = 1; i<=13; i++) {
+                String pin = String(i);
+                if (data.containsKey(pin)) {
+                  pinMode(i, OUTPUT);
+                  int val = body[pin];
+                  digitalWrite(i,val);
+                }
+              }
+              for (int i = 0; i<=7; i++) {
+                String pin = "A";
+                pin = pin + String(i);
+                if (data.containsKey(pin)) {
+                  pinMode(pin.toInt(), OUTPUT);
+                  int val = body[pin];
+                  analogWrite(i,val);
+                }
+              }
             
             // break out of the while loop:
             break;
           } else {    // if you got a newline, then clear currentLine:
-//            Serial.println();
-//            Serial.println(currentLine);
-//            Serial.println();
-                    // Check to see if the client request was "GET /H" or "GET /L":
-//              if (currentLine.startsWith("GET")) {
-//                digitalWrite(13, HIGH);               // GET /H turns the LED on
-//              }
-//              if (currentLine.startsWith("POST")) {
-//                digitalWrite(13, LOW);                // GET /L turns the LED off
-////                int Pin = currentLine.substring(currentLine.indexOf("/"),currentLine.lastIndexOf("/")).toInt();
-//                Serial.print("Pin data read: ");
-//                  
-//              }
+// Look for list of pins that you want to read
+            if (currentLine.startsWith("POST")) {
+              int firstSpace = currentLine.indexOf(' ');
+              int lastSpace = currentLine.lastIndexOf(' ');
+              String readPins = currentLine.substring(firstSpace+2,lastSpace+1);
+              
+                        // Convert from String Object to String.
+              char buf[readPins.length()];
+              readPins.toCharArray(buf, sizeof(buf));
+//              Serial.println(buf);
+
+              int AorD = 0;
+              String pinNum = "";
+
+              for(int i=0;i<(sizeof(buf));i++){
+//                Serial.println(i);
+                if(buf[i] == 'D'){
+                  AorD = 1;
+                  pinNum = "";
+                }
+                else if(buf[i] == 'A'){
+                  AorD = 2;
+                  pinNum = 'A';
+                }
+                else if(buf[i] == '.'){
+                  if(AorD == 1){
+                    String pinBody = "D";
+                    pinBody = pinBody + pinNum;
+                    Serial.println(pinBody);
+                    pinMode(pinNum.toInt(), INPUT);
+                    int val = digitalRead(pinNum.toInt());
+                    doc[pinBody] = val;
+                  }
+                  else if(AorD == 2){
+                    int pin = pinNum.toInt();
+                    Serial.print(pinNum);
+//                    pinMode(pinNum.toInt(), INPUT);
+                    int val = analogRead(pinNum.toInt());
+                    Serial.println(val);
+                    doc[pinNum] = val;
+                  }
+                }
+                else if(buf[i] == NULL) {
+                  break;
+                }
+                else {
+                  pinNum = pinNum + buf[i];
+                }
+              }
+              
+            }
+            else {
+              
+            }
+
             currentLine = "";
             payload = "";
           }
