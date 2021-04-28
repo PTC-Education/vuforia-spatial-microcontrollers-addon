0c63d2df993203af9b489ae73619ead44c6fa32e
jem-gray <60528493+jem-gray@users.noreply.github.com>
2021-04-28 01:19:44 -0400

    added gpio for nano + microbit

--- interfaces/arduinoNanoIoT/index.js
+++ interfaces/arduinoNanoIoT/index.js
@@ -8,8 +8,15 @@ exports.configurable = true;
 
 var objectName = "ArduinoNanoIoT";
 var TOOL_NAME = "ArduinoNanoIoT1";
+var defaultIP = "192.168.50.174";
+var readPins = "D7 D12 A5";
+var analogWrite = "A0 A1 A2 A3";
+var digitalWrite = "D2 D3 D4 D5 D6 D7"
+var readNodeNames = [];
+var readNodeNamesLen = 0;
 
-var defaultIP = "192.168.50.174"
+var digitalWriteNames = [];
+var analogWriteNames = [];
 
 var sensorRefresh = 100;
 
@@ -19,6 +26,7 @@ var accelX;
 var accelY;
 var accelZ;
 var signalStrength;
+var concatReadPins;
 
 //
 // Function to send data from listener nodes and set values for writer nodes
@@ -30,7 +38,7 @@ async function arduinoComm(ip,payload) {
     //     console.log('set default payload')
     //     payload = { '13': 1}; // set default payload
     // }
-    fetch('http://'+ip, { 
+    fetch('http://'+ip+'/'+concatReadPins, { 
         method: 'POST', 
         timeout: 5000,
         body:    JSON.stringify(payload),
@@ -38,7 +46,7 @@ async function arduinoComm(ip,payload) {
     })
     .then(res => res.text())
     .then(json => {
-        // console.log(json)
+        console.log(json)
         return JSON.parse(json)
     })
     .then(body => {
@@ -51,10 +59,16 @@ async function arduinoComm(ip,payload) {
         server.write(objectName, TOOL_NAME, "accelerometerY", accelY, "f");
         server.write(objectName, TOOL_NAME, "accelerometerZ", accelZ, "f");
         server.write(objectName, TOOL_NAME, "signalStrength", signalStrength, "f");
+
+        // loop through read pins and write the values from the nano to the nodes
+        for (var i = 0; i < readNodeNamesLen; ++i) {
+            server.write(objectName, TOOL_NAME, readNodeNames[i], body.readNodeNames[i], "f");
+        }
+
+
     })
     .catch(err => console.error(err));
     setTimeout(() => { arduinoComm(defaultIP,body); }, sensorRefresh);
-    console.log(payload)
 }
 
 
@@ -64,36 +78,59 @@ if (exports.enabled) {
     function setup() {
         exports.settings = {
             nanoName: {
-
                 value: settings('objectName', objectName),
                 type: 'text',
                 default: objectName,
                 disabled: false,
                 helpText: 'The name of the object that connects to this hardware interface'
             },
-            // Complexity level for the object
            nanoIP: { 
                 value: settings('nanoIP', defaultIP), 
                 type: 'text',
                 default: defaultIP,
                 disabled: false,
                 helpText: 'The IP address of your Arduino Nano IoT on the same network as the Vuforia Edge Server'
+            },
+            readPins: {     
+                value: settings('readPins', readPins),
+                type: 'text',
+                default: readPins,
+                disabled: false,
+                helpText: 'The pins you are reading from. Separate each pin with commas and start digital pins with D and analog pins with A'
+            },
+            digitalWrite: {  
+                value: settings('digitalWrite', digitalWrite),
+                type: 'text',
+                default: digitalWrite,
+                disabled: false,
+                helpText: 'Choose which digitalWrite pins to use from the list: D2 D3 D4 D5 D6 D7'
+            },
+            analogWrite: {  
+                value: settings('analogWrite', analogWrite),
+                type: 'text',
+                default: analogWrite,
+                disabled: false,
+                helpText: 'Choose which analogWrite pins to use from the list: A0 A1 A2 A3'
             }
+
         };
     }
-
     objectName = exports.settings.nanoName.value;
     defaultIP = exports.settings.nanoIP.value;
-    console.log("microbit: " + objectName);
-    console.log("with complexity: " + defaultIP);
-
+    readPins = exports.settings.readPins.value;
+    digitalWrite = exports.settings.digitalWrite.value;
+    analogWrite = exports.settings.analogWrite.value;
+    console.log("ArduinoNanoIoT: " + objectName);
+    console.log("with ip: " + defaultIP);
+    console.log("using read pins: " + readPins);
+    console.log("using digital write pins: " + digitalWrite);
+    console.log("using analog write pins: " + analogWrite);
 
     server.addEventListener('reset', function () {
         settings = server.loadHardwareInterface(__dirname);
         setup();
 
         console.log("Microbit: Settings Loaded: ", objectName);
-
     }) 
 }
 
@@ -107,19 +144,116 @@ function startHardwareInterface() {
     server.addNode(objectName, TOOL_NAME, "accelerometerZ", "node");
     server.addNode(objectName, TOOL_NAME, "signalStrength", "node");
     server.addNode(objectName, TOOL_NAME, "led", "node");
-    
+
+    // creates a string that represents the pins to read from
+    // this string is concatenated with the URL to be sent to the arduino 
+    console.log("readPins: " + readPins);
+    concatReadPins = readPins.replace(/ /g, ".");
+    console.log("new readPins: " + concatReadPins);
+    concatReadPins = concatReadPins.concat('.');
+    console.log("final readPins: " + concatReadPins);
+
+    // adds each read pin node to the server
+    readNodeNames = readPins.split(" ");
+    readNodeNamesLen = readNodeNames.length;
+    for (var i = 0; i < readNodeNamesLen; ++i) {
+        server.addNode(objectName, TOOL_NAME, readNodeNames[i], "node");
+    }
+
+    // adds each digital write pin node to the server
+    digitalWriteNames = digitalWrite.split(" ");
+    var digitalWritelLen = digitalWriteNames.length;
+    for (var i = 0; i < digitalWritelLen; ++i) {
+        console.log("digital write names: " + digitalWriteNames[i]);
+        server.addNode(objectName, TOOL_NAME, digitalWriteNames[i], "node");
+    }
+
+    // adds each analog write pin node to the server
+    analogWriteNames = analogWrite.split(" ");
+    var analogWritelLen = analogWriteNames.length;
+    for (var i = 0; i < analogWritelLen; ++i) {
+        console.log("analog write names: " + analogWriteNames[i]);
+        server.addNode(objectName, TOOL_NAME, analogWriteNames[i], "node");
+    }
+
+    var dataVal1, dataVal2, dataVal3, dataVal4, dataVal5, dataVal6;
+    var pinNum1, pinNum2, pinNum3, pinNum4, pinNum5, pinNum6;
+    var tempBody;
+
+    // gets the pin number for the first digital pin
+    var nameLen1 = digitalWriteNames[0].length;
+    pinNum1 = digitalWriteNames[0].substring(1, nameLen1);
+    console.log("the pin num is: " + pinNum1);
+
+    // read listener for first digital pin - creates a JSON obj to hold the data value
+    server.addReadListener(objectName, TOOL_NAME, digitalWriteNames[0], function(data) {
+        console.log("in digital write read listener");
+        dataVal1 = data.value;
+    });
+
+    // gets the pin number for the second digital pin
+    var nameLen2 = digitalWriteNames[1].length;
+    pinNum2 = digitalWriteNames[1].substring(1, nameLen2);
+    console.log("the pin num is: " + pinNum2);
+
+    // read listener for second digital pin - creates a JSON obj to hold the data value
+    server.addReadListener(objectName, TOOL_NAME, digitalWriteNames[1], function(data) {
+        console.log("in digital write read listener");
+        dataVal2 = data.value;
+    });
+
+    // gets the pin number for the third digital pin
+    var nameLen3 = digitalWriteNames[2].length;
+    pinNum3 = digitalWriteNames[2].substring(1, nameLen3);
+    console.log("the pin num is: " + pinNum3);
+
+    // read listener for third digital pin - creates a JSON obj to hold the data value
+    server.addReadListener(objectName, TOOL_NAME, digitalWriteNames[2], function(data) {
+        console.log("in digital write read listener");
+        dataVal3 = data.value;
+    });
+
+    // // gets the pin number for the fourth digital pin
+    // var nameLen4 = digitalWriteNames[3].length;
+    // pinNum4 = digitalWriteNames[3].substring(1, nameLen4);
+    // console.log("the pin num is: " + pinNum4);
+
+    // // read listener for fourth digital pin - creates a JSON obj to hold the data value
+    // server.addReadListener(objectName, TOOL_NAME, digitalWriteNames[3], function(data) {
+    //     console.log("in digital write read listener");
+    //     dataVal4 = data.value;
+    // });
+
+    // // gets the pin number for the fifth digital pin
+    // var nameLen5 = digitalWriteNames[4].length;
+    // pinNum5 = digitalWriteNames[4].substring(1, nameLen5);
+    // console.log("the pin num is: " + pinNum5);
+
+    // // read listener for fifth digital pin - creates a JSON obj to hold the data value
+    // server.addReadListener(objectName, TOOL_NAME, digitalWriteNames[4], function(data) {
+    //     console.log("in digital write read listener");
+    //     dataVal5 = data.value;
+    // });
+
+    // // gets the pin number for the sixth digital pin
+    // var nameLen6 = digitalWriteNames[5].length;
+    // pinNum6 = digitalWriteNames[5].substring(1, nameLen6);
+    // console.log("the pin num is: " + pinNum6);
+
+    // // read listener for sixth digital pin - creates a JSON obj to hold the data value
+    // server.addReadListener(objectName, TOOL_NAME, digitalWriteNames[5], function(data) {
+    //     console.log("in digital write read listener");
+    //     dataVal6 = data.value;
+    // });
+
     server.addReadListener(objectName, TOOL_NAME, "led", function(data){
         console.log("in read listener");
-        if (data.value == 1) {
-            // console.log("data is 1");
-            setTimeout(() => { body = { '13': 1}; } , 0);
-        }
-        else if (data.value == 0) {
-            // console.log("data is 0");
-            setTimeout(() => { body = { '13': 0}; } , 0);
-            // console.log("wrote to port");
-        }
+        tempBody = data.value;
     });
+
+    // NOTE: Might have to check format of body!!
+    body = {'13' : tempBody, [pinNum1] : dataVal1, [pinNum2] : dataVal2, [pinNum3] : dataVal3/*, [pinNum4] : dataVal4, [pinNum5] : dataVal5, [pinNum6] : dataVal6*/};
+    console.log(body);
 
     arduinoComm(defaultIP,body);
 
@@ -139,4 +273,3 @@ server.addEventListener("initialize", function () {
         startHardwareInterface();
     }, 1000)
 });
-
--- interfaces/microbit/index.js
+++ interfaces/microbit/index.js
@@ -9,6 +9,19 @@ var objectName = "microbit";
 var TOOL_NAME = "MicroBit1";
 var complexity = "MicroBit1"
 
+var analogRead = "A0 A1 A2 A3";
+var analogWrite = "A4 A5 A6 A7";
+var digitalWrite = "D8 D9 D10 D11 D12";
+var digitalRead = "D13 D14 D15 D16";
+var readPins = "";
+var writePins = "";
+var readDigitalNames = [];
+var readAnalogNames = [];
+var writeDigitalNames = [];
+var writeAnalogNames = [];
+var analogArr = [];
+
+
 var sensorRefresh = 50;
 
 serial.openPort();
@@ -33,14 +46,54 @@ if (exports.enabled) {
                 disabled: false,
                 helpText: 'The complexity of the interface. "beginner" gives a few nodes, If you want super accurate sensor data, \
                 you can use the complexity "sensor" to get faster sensor data in exchange for no motor control.'
+            },
+            analogRead: {
+                value: settings('analogRead', analogRead),
+                type: 'text',
+                default: analogRead,
+                disabled: false,
+                helpText: 'Choose which analogRead pins to use from the list: A0 A1 A2 A3'
+            },
+            analogWrite: {
+                value: settings('analogWrite', analogWrite),
+                type: 'text',
+                default: analogWrite,
+                disabled: false,
+                helpText: 'Choose which analogWrite pins to use from the list: A4 A5 A6 A7'
+            },
+            digitalWrite: {
+                value: settings('digitalWrite', digitalWrite),
+                type: 'text',
+                default: digitalWrite,
+                disabled: false,
+                helpText: 'Choose which digitalWrite pins to use from the list: D13 D14 D15 D16'
+            },
+            digitalRead: {
+                value: settings('digitalRead', digitalRead),
+                type: 'text',
+                default: digitalRead,
+                disabled: false,
+                helpText: 'Choose which digitalRead pins to use from the list: D8 D9 D10 D11 D12'
             }
         };
     }
     objectName = exports.settings.microbitName.value;
     complexity = exports.settings.microbitComplexity.value.toLowerCase();
     complexity = complexity.replace(/\n/g,'');
+    analogRead = exports.settings.analogRead.value;
+    analogRead = analogRead.replace(/\n/g,'');
+    analogWrite = exports.settings.analogWrite.value;
+    analogWrite = analogWrite.replace(/\n/g,'');
+    digitalRead = exports.settings.digitalRead.value;
+    digitalRead = digitalRead.replace(/\n/g,'');
+    digitalWrite = exports.settings.digitalWrite.value;
+    digitalWrite = digitalWrite.replace(/\n/g,'');
     console.log("microbit: " + objectName);
     console.log("with complexity: " + complexity);
+    console.log("using analogRead pins: " + analogRead);
+    console.log("using analogWrite pins: " + analogWrite);
+    console.log("using digitalRead pins: " + digitalRead);
+    console.log("using digitalWrite pins: " + digitalWrite);
 
     server.addEventListener('reset', function () {
         settings = server.loadHardwareInterface(__dirname);
@@ -56,12 +109,12 @@ function startHardwareInterface() {
     server.enableDeveloperUI(true)
 
     if (complexity == 'sensor' || complexity == 'beginner') {
-        server.addNode(objectName, TOOL_NAME, "accelerometerX", "node", {x: 60, y: -20, scale:0.135});
-        server.addNode(objectName, TOOL_NAME, "buttonA", "node", {x: -50, y: -40, scale:0.135});
-        server.addNode(objectName, TOOL_NAME, "buttonB", "node", {x: -50, y: -20, scale:0.135});
+        server.addNode(objectName, TOOL_NAME, "accelerometerX", "node", {x: 60, y: -30, scale:0.125});
+        server.addNode(objectName, TOOL_NAME, "buttonA", "node", {x: -50, y: -60, scale:0.125});
+        server.addNode(objectName, TOOL_NAME, "buttonB", "node", {x: -50, y: -30, scale:0.125});
 
         if (complexity == 'beginner') {
-            server.addNode(objectName, TOOL_NAME, "screen", "node", {x: 60, y: 0, scale:0.135});
+            server.addNode(objectName, TOOL_NAME, "screen", "node", {x: 60, y: -60, scale:0.125});
 
             server.removeNode(objectName, TOOL_NAME, "accelerometerY");
             server.removeNode(objectName, TOOL_NAME, "accelerometerZ");
@@ -74,21 +127,128 @@ function startHardwareInterface() {
         if (complexity == 'sensor') {
             server.removeNode(objectName, TOOL_NAME, "screen");
 
-            server.addNode(objectName, TOOL_NAME, "accelerometerY", "node", {x: 60, y: 0, scale:0.135});
-            server.addNode(objectName, TOOL_NAME, "accelerometerZ", "node", {x: 60, y: 20, scale:0.135});
-            server.addNode(objectName, TOOL_NAME, "brightness", "node", {x: -50, y: 0, scale:0.135});
-            server.addNode(objectName, TOOL_NAME, "temp", "node", {x: -50, y: 20, scale:0.135});
+            server.addNode(objectName, TOOL_NAME, "accelerometerY", "node", {x: 60, y: 0, scale:0.125});
+            server.addNode(objectName, TOOL_NAME, "accelerometerZ", "node", {x: 60, y: 30, scale:0.125});
+            server.addNode(objectName, TOOL_NAME, "brightness", "node", {x: -50, y: 0, scale:0.125});
+            server.addNode(objectName, TOOL_NAME, "temp", "node", {x: -50, y: 30, scale:0.125});
 
             sensorRefresh = 10;
         }
     }
-    
+
+    readPins = digitalRead + " " + analogRead;
+    writePins = digitalWrite + " " + analogWrite;
+    console.log("read pins: " + readPins);
+    console.log("write pins: " + writePins);
+    readNames = readPins.split(" ");
+    writeNames = writePins.split(" ");
+    console.log("read names: " + readNames);
+    console.log("write names: " + writeNames);
+
+    readDigitalNames =  digitalRead.split(" ");
+    var readDigitalLen = readDigitalNames.length;
+    for (var i = 0; i < readDigitalLen; ++i) {
+        readDigitalNames[i] = "R".concat(readDigitalNames[i])
+        server.addNode(objectName, TOOL_NAME, readDigitalNames[i], "node");
+    }
+
+    writeDigitalNames =  digitalWrite.split(" ");
+    var writeDigitalLen = writeDigitalNames.length;
+    for (var i = 0; i < writeDigitalLen; ++i) {
+        writeDigitalNames[i] = "W".concat(writeDigitalNames[i]);
+        server.addNode(objectName, TOOL_NAME, writeDigitalNames[i], "node");
+    }
+
+    readAnalogNames =  analogRead.split(" ");
+    var readAnalogLen = readAnalogNames.length;
+    for (var i = 0; i < readAnalogLen; ++i) {
+        readAnalogNames[i] = "R".concat(readAnalogNames[i])
+        server.addNode(objectName, TOOL_NAME, readAnalogNames[i], "node");
+    }
+
+    writeAnalogNames =  analogWrite.split(" ");
+    var writeAnalogLen = writeAnalogNames.length;
+    for (var i = 0; i < writeAnalogLen; ++i) {
+        writeAnalogNames[i] = "W".concat(writeAnalogNames[i]);
+        server.addNode(objectName, TOOL_NAME, writeAnalogNames[i], "node");
+    }
+
+    var analogTemp = analogWrite.replace(/A/g,'P');
+    var analogTempArr = analogTemp.split(" ");
+
+    var name1 = analogTempArr[0];
+    server.addReadListener(objectName, TOOL_NAME, writeAnalogNames[0], function(data){
+        var writeAnalogVal = data.value + name1 + ",";
+        console.log("analog val within loop: " + writeAnalogVal);
+        setTimeout(() => { serial.writePort(writeAnalogVal); } , 0);
+    });
+
+    var name2 = analogTempArr[1];
+    server.addReadListener(objectName, TOOL_NAME, writeAnalogNames[1], function(data){
+        var writeAnalogVal = data.value + name2 + ",";
+        console.log("analog val within loop: " + writeAnalogVal);
+        setTimeout(() => { serial.writePort(writeAnalogVal); } , 0);
+    });
+
+    var name3 = analogTempArr[2];
+    server.addReadListener(objectName, TOOL_NAME, writeAnalogNames[2], function(data){
+        var writeAnalogVal = data.value + name3 + ",";
+        console.log("analog val within loop: " + writeAnalogVal);
+        setTimeout(() => { serial.writePort(writeAnalogVal); } , 0);
+    });
+
+    var name4 = analogTempArr[3];
+    server.addReadListener(objectName, TOOL_NAME, writeAnalogNames[3], function(data){
+        var writeAnalogVal = data.value + name4 + ",";
+        console.log("analog val within loop: " + writeAnalogVal);
+            setTimeout(() => { serial.writePort(writeAnalogVal); } , 0);
+    });
+
+
+    var digitalTemp = digitalWrite.replace(/D/g, 'P');
+    var digitalTempArr = digitalTemp.split(" ");
+
+    var dname1 = digitalTempArr[0];
+    console.log(writeDigitalNames[0]);
+    server.addReadListener(objectName, TOOL_NAME, writeDigitalNames[0], function(data) {
+        var writeDigitalVal = data.value + dname1 + ",";
+        console.log("digital val within loop: " + writeDigitalVal);
+        setTimeout(() => { serial.writePort(writeDigitalVal); } , 0);
+    });
+
+    var dname2 = digitalTempArr[1];
+    server.addReadListener(objectName, TOOL_NAME, writeDigitalNames[1], function(data) {
+        var writeDigitalVal = data.value + dname2 + ",";
+        console.log("digital val within loop: " + writeDigitalVal);
+        setTimeout(() => { serial.writePort(writeDigitalVal); } , 0);
+    });
+
+    var dname3 = digitalTempArr[2];
+    server.addReadListener(objectName, TOOL_NAME, writeDigitalNames[2], function(data) {
+        var writeDigitalVal = data.value + dname3 + ",";
+        console.log("digital val within loop: " + writeDigitalVal);
+        setTimeout(() => { serial.writePort(writeDigitalVal); } , 0);
+    });
+
+    var dname4 = digitalTempArr[3];
+    server.addReadListener(objectName, TOOL_NAME, writeDigitalNames[3], function(data) {
+        var writeDigitalVal = data.value + dname4 + ",";
+        console.log("digital val within loop: " + writeDigitalVal);
+        setTimeout(() => { serial.writePort(writeDigitalVal); } , 0);
+    });
+
+    var dname5 = digitalTempArr[4];
+    server.addReadListener(objectName, TOOL_NAME, writeDigitalNames[4], function(data) {
+        var writeDigitalVal = data.value + dname5 + ",";
+        console.log("digital val within loop: " + writeDigitalVal);
+        setTimeout(() => { serial.writePort(writeDigitalVal); } , 0);
+    });  
 
     server.addReadListener(objectName, TOOL_NAME, "screen", function(data){
-        console.log("in read listener");
+    	// console.log("in read listener");
 
         var info = String(data.value);
-        var end = ",";
+        var end = "#,";
         var val = info.concat(end);
         setTimeout(() => { serial.writePort(val); }, 0);
     });
@@ -101,12 +261,18 @@ function startHardwareInterface() {
 
 function updateEvery(i, time){
     setTimeout(() => {
-        updateEvery(++i, time);
+    	updateEvery(++i, time);
     }, time)
+}
+
+function initAnalogWriteArr(arr) {
+    arr = writeAnalogNames;
+    console.log("names within func " + arr);
 }
 
 async function readSensor() {
     var sensorData = serial.getSensor();
+    // console.log(parseInt(sensorData[0]) + " " + parseInt(sensorData[7]));
     
     server.write(objectName, TOOL_NAME, "temp", parseInt(sensorData[0]), "f");
     server.write(objectName, TOOL_NAME, "accelerometerX", parseInt(sensorData[1]), "f");
@@ -115,6 +281,16 @@ async function readSensor() {
     server.write(objectName, TOOL_NAME, "brightness", parseInt(sensorData[4]), "f");
     server.write(objectName, TOOL_NAME, "buttonA", parseInt(sensorData[5]), "f");
     server.write(objectName, TOOL_NAME, "buttonB", parseInt(sensorData[6]), "f");
+
+    var readDigitalLen = readDigitalNames.length;
+    for (var i = 0; i < readDigitalLen; ++i) {
+        server.write(objectName, TOOL_NAME, readDigitalNames[i], parseInt(sensorData[i + 7]), "f");
+    }
+
+    var readAnalogLen = readAnalogNames.length;
+    for (var i = 0; i < readAnalogLen; ++i) {
+        server.write(objectName, TOOL_NAME, readAnalogNames[i], parseInt(sensorData[i + 12]), "f");
+    }
 
     setTimeout(() => { readSensor(); }, sensorRefresh);
 }
